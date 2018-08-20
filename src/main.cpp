8934cdf340cc4ffba6678e3674ee05a01f6c9356
Matthias Bock <mail@matthiasbock.net>
2018-08-20 14:21:29 +0200

    main.cpp: Using objects instead of pointers to objects

--- src/main.cpp
+++ src/main.cpp
@@ -20,30 +20,27 @@ using namespace std::chrono_literals;
 
 int main()
 {
-    USBInterface* usb = new USBInterface(
-                                Yokogawa::WT3000::USB::VID,
-                                Yokogawa::WT3000::USB::PID,
-                                Yokogawa::WT3000::USB::EndpointTransmit,
-                                Yokogawa::WT3000::USB::EndpointReceive
-                                );
-    usb->setLogLevel(LogLevel::Debug);
+    USBInterface usb(
+                    Yokogawa::WT3000::USB::VID,
+                    Yokogawa::WT3000::USB::PID,
+                    Yokogawa::WT3000::USB::EndpointTransmit,
+                    Yokogawa::WT3000::USB::EndpointReceive
+                    );
+    usb.setLogLevel(LogLevel::Debug);
 
-    if (!usb->isOpen())
+    if (!usb.isOpen())
     {
         return -1;
     }
 
-    Yokogawa::WT3000::Interface* analyzer = new Yokogawa::WT3000::Interface(usb);
-    analyzer->connect();
+    Yokogawa::WT3000::Interface analyzer(&usb);
+    analyzer.connect();
 
-    analyzer->setNumericFormat(Yokogawa::WT3000::GPIB::Numeric::Format::Float);
+    analyzer.setNumericFormat(Yokogawa::WT3000::GPIB::Numeric::Format::Float);
 
     for (uint8_t i=0; i<1; i++)
     {
-        analyzer->getNumericValuesAsFloats();
+        analyzer.getNumericValuesAsFloats();
         this_thread::sleep_for(1s);
     }
-
-    delete analyzer;
-    delete usb;
 }
